29573a8b55325e5711bea40fad3bfb10c0196963
Mark Aster <56190468+mark-aster1@users.noreply.github.com>
2026-07-22 22:13:31 +0300

    Initialize BLE device and start advertising

--- Scripts/Module Code/BLE_LOCALIZER/CARRIER.ino
+++ Scripts/Module Code/BLE_LOCALIZER/CARRIER.ino
@@ -1,1 +1,16 @@
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
 
+void setup() {
+  BLEDevice::init("CARRIER_BEACON");
+
+  BLEServer *pServer = BLEDevice::createServer();
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+
+  pAdvertising->start();
+}
+
+void loop() {
+  delay(50);
+}
